8aa1045c64a8c5c116b8e791970e68978a874875
juliochandra <juliochandra@gmail.com>
2026-08-27 10:46:17 +0700

    fix bug

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
+import Loading from "../components/Loading";
 
-const Layout = () => {
-	return (
-		<>
-			<Outlet />
-		</>
-	);
+export const AuthLayout = () => {
+	const { user, loadingUser } = useAppContext();
+
+	if (loadingUser) return <Loading />;
+	if (!user) return <Navigate to="/login" replace />;
+
+	return <Outlet />;
 };
-export default Layout;
+
+export const GuestLayout = () => {
+	const { user, loadingUser } = useAppContext();
+
+	if (loadingUser) return <Loading />;
+	if (user) return <Navigate to="/" replace />;
+
+	return <Outlet />;
+};
